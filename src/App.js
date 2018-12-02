6df8022a04c1b2f341f80c655469e8982a9e27e3
tarekhassan410 <tarekhassan410@gmail.com>
2018-12-02 14:49:00 +0200

    new edits

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./bulma_theme.css";
+import axios from "axios";
 
 class App extends Component {
+  state = {
+    shortcuts: [],
+    source: 'data',
+    value: null
+  };
+
+  constructor(){
+    super()
+    this.filterList = this.filterList.bind(this)
+  }
+
+  componentDidMount() {
+    this.getData()
+  }
+
+  getData(){
+    axios.get(`./` + this.state.source + `.json`).then(response => {
+      this.setState({
+        shortcuts: response.data.windows
+      });
+    });
+  }
+
+  filterList(e){
+    this.setState({
+      value: e.target.value 
+    })
+    
+  }
+
   render() {
+
+    const list = this.state.value == null || '' ? this.state.shortcuts : this.state.shortcuts.filter( shortcut => shortcut.command.includes(this.state.value) && shortcut.command  )
+    // this.state.shortcuts.filter( shortcut => shortcut.command.includes(e.target.value) && shortcut.command  )
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div className="hero section is-small">
+          <div className="container has-text-centered">
+            <div className="field">
+              <p className="control has-icons-left has-icons-right">
+                <input onChange={this.filterList} className="input is-primary is-radiusless" type="email" placeholder="Search commands 'cut', 'copy' etc " />
+                <span className="icon is-small is-left">
+                  <i className="fas fa-search" />
+                </span>
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="section">
+          <div className="container">
+            <table className="table is-fullwidth is-striped is-hoverable">
+              <thead className="">
+                <tr>
+                  <th>Key</th>
+                  <th>Command</th>
+                  <th>3</th>
+                </tr>
+              </thead>
+              <tbody>
+                {list.map((s, i) => (
+                  <tr key={i} className="menu-list">
+                    <td>{s.key}</td>
+                    <td>{s.command}</td>
+                    <td>f</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     );
   }
